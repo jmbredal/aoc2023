9ba291f485ef515a9bc666d50142f7f93bd2cb1e
John-Magne Bredal <bredal@jmmi.no>
2023-12-04 21:48:44 +0100

    Solve day4

--- day04/index.js
+++ day04/index.js
@@ -4,17 +4,42 @@ const inputFile = 'input.txt';
 const test1File = 'test1.txt';
 const test2File = 'test2.txt';
 
+const getScore = card => Math.pow(2, card.length - 1);
+const parse = (line) => line.split(':')[1].split('|').map(l => l.trim().split(/\s+/).toNumbers());
+const getWinning = card => [...new Set(card[0]).intersection(new Set(card[1]))];
+
 function solve1(input) {
-  const lines = readLines(input);
+  return readLines(input).map(parse).map(getWinning).filter(x => x.length > 0).map(getScore).sum();
 }
 
 function solve2(input) {
-  const lines = readLines(input);
+  const cards = readLines(input)
+    .map(parse)
+    .map(getWinning)
+    .map((x, index) => ({
+      winningNums: x.length,
+      index
+    }));
+
+  function count(card, counter) {
+    counter++;
+
+    const startIndex = card.index + 1;
+    for (let index = startIndex; index < startIndex + card.winningNums; index++) {
+      const _card = cards[index];
+      if (_card) {
+        counter = count(_card, counter);
+      }
+    }
+
+    return counter;
+  }
+
+  return cards.map((c, i) => count(cards[i], 0)).sum();
 };
 
 console.log('-----------');
-console.log(solve1(test1File));
+// console.log(solve1(test1File));
 // console.log(solve1(inputFile));
-// console.log(solve2(test1File));
-// console.log(solve2(inputFile));
-
+console.log(solve2(test1File));
+console.log(solve2(inputFile));
