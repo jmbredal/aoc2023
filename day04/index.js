7bd60cfa5db29629e34b78c7750b6942426acfb9
John-Magne Bredal <bredal@jmmi.no>
2023-12-04 16:15:06 +0100

    Init day04

---
+++ day04/index.js
@@ -0,0 +1,20 @@
+import { readLines } from '../utils.js';
+
+const inputFile = 'input.txt';
+const test1File = 'test1.txt';
+const test2File = 'test2.txt';
+
+function solve1(input) {
+  const lines = readLines(input);
+}
+
+function solve2(input) {
+  const lines = readLines(input);
+};
+
+console.log('-----------');
+console.log(solve1(test1File));
+// console.log(solve1(inputFile));
+// console.log(solve2(test1File));
+// console.log(solve2(inputFile));
+
